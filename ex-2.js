d77ebaa1c3569efce3ee41fd1a83b4a3f5e9808d
nattawara <nattawara.jtk@gmail.com>
2024-05-15 13:22:16 +0700

    ploy/equality assignment

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,7 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+console.log(0 == false); //true
+console.log(null == undefined); //true
+console.log(10 == "10"); //true
+console.log([10] == 10); //true
+console.log([1, 2] == "1,2"); //true
